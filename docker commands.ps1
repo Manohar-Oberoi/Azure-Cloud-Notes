7477f35b424c50e4683020c1235b2b41429a60cc
mahesh kumar <maheshkumar0091989@gmail.com>
2025-01-21 13:21:47 +0530

    docker command file added

---
+++ docker commands.ps1
@@ -0,0 +1,13 @@
+﻿docker --version
+docker pull nginx
+docker images
+docker run -it -p 8080:80 nginx
+docker tag nginx nginxcontainerregistry.azurecr.io/samples/nginx
+az login
+az acr login --name nginxcontainerregistry
+docker push nginxcontainerregistry.azurecr.io/samples/nginx
+docker container list
+docker stop c480c3f5d40a
+docker remove c480c3f5d40a
+docker rmi nginx
+docker rmi nginxcontainerregistry.azurecr.io/samples/nginx
